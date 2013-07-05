4cf7f36d4ce4a7b1499614829b20db5a48a7c5e4
Seraf <julien.syx@gmail.com>
2013-07-05 15:39:42 +0200

    Adding translations for the plugin

--- tests/chat_test.py
+++ tests/chat_test.py
@@ -2,11 +2,12 @@ import json, os, sys
 from twisted.trial import unittest
 from twisted.test import proto_helpers
 sys.path.append(os.path.normpath(os.path.join(os.path.abspath("../../../"))))
-from lisa import LisaFactory, configuration
+import libs
+from lisa import configuration
 
 class ChatTestCase(unittest.TestCase):
     def setUp(self):
-        factory = LisaFactory()
+        factory = libs.LisaInstance
         self.proto = factory.buildProtocol(('127.0.0.1', 0))
         self.tr = proto_helpers.StringTransport()
         self.proto.makeConnection(self.tr)
@@ -32,3 +33,6 @@ class ChatTestCase(unittest.TestCase):
         if configuration['lang'] == 'fr':
             now = datetime.now()
             return self._test(sentence='il est quelle heure', expected=now.strftime("Il est %H heures et %M minutes"))
+        elif configuration['lang'] == 'en':
+            now = datetime.now()
+            return self._test(sentence='what time is it', expected=now.strftime("It is %H:%M%p"))
