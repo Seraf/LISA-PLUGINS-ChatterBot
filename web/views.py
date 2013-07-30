af922ef5a7ec62aa994e2fc6b1139996619f056a
Seraf <julien.syx@gmail.com>
2013-07-30 10:22:47 +0200

    Adding views and templates

--- web/views.py
+++ web/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.contrib.auth.decorators import login_required
+import os, json
 try:
     from web.lisa.utils import method_restricted_to, is_ajax
     from web.lisa.settings import LISA_PATH
@@ -10,11 +11,11 @@ except ImportError:
 
 @login_required()
 def widget1(request, x, y):
-    from ChatterBot.module.chat import Chat
-    content = Chat().getTime()
-    return render_to_response('widget.html',
+    from ChatterBot.modules.chat import Chat
+    content = json.loads(Chat().getTime())
+    return render_to_response(os.path.abspath(os.path.dirname(__file__) + '/templates/widget.html'),
                               {
-                                  'content': content, 'data-sizex': "1", 'data-sizey': "1",
+                                  'content': str(content['body']), 'data_sizex': "1", 'data_sizey': "1",
                                   'data-row': x, 'data-col': y
                               },
                               context_instance=RequestContext(request))
