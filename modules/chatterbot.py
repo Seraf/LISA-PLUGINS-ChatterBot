6ec24a5d5453db2b0b2885cceb17e73edf21bc09
Seraf <julien.syx@gmail.com>
2013-08-12 17:41:34 +0200

    Fixing old module

--- modules/chatterbot.py
+++ modules/chatterbot.py
@@ -10,14 +10,22 @@ path = os.path.realpath(os.path.abspath(os.path.join(os.path.split(
     inspect.getfile(inspect.currentframe()))[0],os.path.normpath("../lang/"))))
 _ = translation = gettext.translation(domain='chat', localedir=path, languages=[configuration['lang']]).ugettext
 
-class Chat:
+class ChatterBot:
     def __init__(self):
         self.configuration_lisa = configuration
         mongo = MongoClient(host=self.configuration_lisa['database']['server'],
                             port=self.configuration_lisa['database']['port'])
         self.configuration = mongo.lisa.plugins.find_one({"name": "ChatterBot"})
 
-    def getTime(self):
+    def getTime(self, jsonInput):
         now = datetime.now()
-        return json.dumps({"plugin": "ChatterBot","method": "getTime",
-                           "body": now.strftime(_('time'))})
+        return {"plugin": "ChatterBot",
+                "method": "getTime",
+                "body": now.strftime(_('time'))
+        }
+
+    def sayHello(self, jsonInput):
+        return {"plugin": "ChatterBot",
+                "method": "sayHello",
+                "body": _('Hello. How are you ?')
+        }
